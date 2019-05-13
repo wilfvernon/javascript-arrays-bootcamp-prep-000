691e02d09949b793c0ab0219cfe00a522f32e4f9
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-13 16:23:13 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,35 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return function callNewArray(){
+    console.log(array)
+  }
+}
+function addElementToBeginningOfArray(array, element){
+  newarray =  [element,...array]
+  return function callNewArray(){
+    console.log(newarray)
+  }
+}
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return function callNewArray(){
+    console.log(array)
+  }
+}
+function addElementToEndOfArray(array, element){
+  newarray =  [...array, element]
+  return function callNewArray(){
+    console.log(newarray)
+  }
+}
+function accessElementInArray(array, index){
+  return array[index]
+  return function callIndex(){
+    console.log(array[index])
+  }
+}
+
+accessElementInArray(chocolateBars, 3)
+addElementToEndOfArray(chocolateBars, "rolos")()
